014a8765f2a8cce11cae5d920bf1c65cf5a5209f
Chad Botting <coffeeprogrammer0@gmail.com>
2021-12-10 13:03:29 -0500

    first commit

---
+++ numerals/Numeral.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace numerals
+{
+    public class Numeral
+    {
+        private string _romanRepresentation;
+        private int _deciamalRepresentation;
+
+        public Numeral()
+        {
+
+        }
+
+        public string Roman
+        {
+            get
+            {
+                return _romanRepresentation;
+            }
+            set
+            {
+                _romanRepresentation = value;
+            }
+        }
+
+
+        private int ConvertToDeciamal(string roman)
+        {
+
+            int returnValue = 0;
+            int length = roman.Length;
+
+
+            return returnValue;
+        }
+
+        private int GetValueFromRomanDigit(char rd)
+        {
+            int returnValue = -1;
+
+            if (rd == 'M' || rd == 'm')
+                returnValue = 1000;
+            if (rd == 'D' || rd == 'd')
+                returnValue = 500;
+            if (rd == 'C' || rd == 'c')
+                returnValue = 100;
+            if (rd == 'L' || rd == 'l')
+                returnValue = 50;
+            if (rd == 'X' || rd == 'x')
+                returnValue = 10;
+            if (rd == 'V' || rd == 'v')
+                returnValue = 5;
+            if (rd == 'I' || rd == 'i')
+                returnValue = 1;
+
+            return returnValue;
+        }
+
+    }
+}
